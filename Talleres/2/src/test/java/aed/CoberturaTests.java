099d35569884ad8cc868b2bcb05ed393f3bd7918
Maximiliano Fisz <mfisz@galmesdigital.com>
2023-08-26 19:53:37 -0300

    asdasd

--- Talleres/2/src/test/java/aed/CoberturaTests.java
+++ Talleres/2/src/test/java/aed/CoberturaTests.java
@@ -11,12 +11,18 @@ class CoberturaTests {
 
     @Test
     void testFizzBuzz() {
-        assertEquals("A","B");
+        assertEquals("FizzBuzz", cobertura.fizzBuzz(15));
+        assertEquals("Fizz", cobertura.fizzBuzz(6));
+        assertEquals("Buzz", cobertura.fizzBuzz(10));
+        assertEquals("7", cobertura.fizzBuzz(7));
+
     }
 
     @Test
     void testNumeroCombinatorio() {
-        assertTrue(false);
+        assertEquals(1, cobertura.numeroCombinatorio(1,1));
+        assertEquals(4, cobertura.numeroCombinatorio(4,3));
+         assertEquals(1, cobertura.numeroCombinatorio(2,2));
     }
 
     @Test
